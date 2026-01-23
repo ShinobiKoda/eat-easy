c64be23a87e979069955771f167e69028e2dc462
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-01-23 16:33:56 +0100

    feat: add ScaleButton component and integrate animations in Welcome component

--- src/components/dashboard/Welcome.tsx
+++ src/components/dashboard/Welcome.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
 import Header from "../layout/Header";
 import { FaArrowRight } from "react-icons/fa";
-
-import Loader from "../Loader";
+import {
+  PopIn,
+  MotionContainer,
+  FadeIn,
+  ScaleButton,
+} from "../animations/motion";
 
 const options = [
   {
@@ -19,53 +21,39 @@ const options = [
 ];
 
 const Welcome: React.FC = () => {
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    const t = setTimeout(() => setShowLoader(false), 3000);
-    return () => clearTimeout(t);
-  }, []);
-
   return (
     <div className="w-full min-h-screen">
-      {showLoader && <Loader />}
-
-      <div
-        className={`transition-all duration-300 ${
-          showLoader ? "pointer-events-none overflow-hidden" : ""
-        }`}
-      >
+      <MotionContainer className={`transition-all duration-300 `}>
         <Header description="Browse Our Food Menu" navbarTitle="Gbam Gbistro" />
         <div className="w-full pt-[60px] px-6">
-          <h1 className="heading-font text-(--neutral-800) font-medium text-[22px] text-center dark:text-white">
+          <PopIn className="heading-font text-(--neutral-800) font-medium text-[22px] text-center dark:text-white">
             Let's find the perfect dish for you
-          </h1>
+          </PopIn>
 
-          <div className="mt-6 space-y-6">
+          <MotionContainer className="mt-6 space-y-6">
             {options.map((option, index) => (
-              <div
-                key={index}
-                className="p-5 rounded-2xl bg-white shadow-md space-y-5 dark:bg-(--neutral-700)"
-              >
-                <img src={option.icon} alt="Option Icon" />
-                <div className="flex justify-between">
-                  <div className="space-y-[23px] max-w-[225px]">
-                    <h3 className="font-semibold text-base text-(--neutral-900) dark:text-white">
-                      {option.title}
-                    </h3>
-                    <p className="text-(--neutral-500) font-medium text-sm dark:text-(--neutral-300)">
-                      {option.description}
-                    </p>
-                  </div>
-                  <div className="w-[46px] h-[46px] rounded-xl bg-(--orange-5) dark:bg-(--orange-1) flex items-center justify-center mt-auto">
-                    <FaArrowRight className="text-(--orange-1) dark:text-white" />
+              <FadeIn key={index}>
+                <div className="p-5 rounded-2xl bg-white shadow-md space-y-5 dark:bg-(--neutral-700)">
+                  <img src={option.icon} alt="Option Icon" />
+                  <div className="flex justify-between">
+                    <div className="space-y-[23px] max-w-[225px]">
+                      <h3 className="font-semibold text-base text-(--neutral-900) dark:text-white">
+                        {option.title}
+                      </h3>
+                      <p className="text-(--neutral-500) font-medium text-sm dark:text-(--neutral-300)">
+                        {option.description}
+                      </p>
+                    </div>
+                    <ScaleButton className="w-[46px] h-[46px] rounded-xl bg-(--orange-5) dark:bg-(--orange-1) flex items-center justify-center mt-auto">
+                      <FaArrowRight className="text-(--orange-1) dark:text-white" />
+                    </ScaleButton>
                   </div>
                 </div>
-              </div>
+              </FadeIn>
             ))}
-          </div>
+          </MotionContainer>
         </div>
-      </div>
+      </MotionContainer>
     </div>
   );
 };
