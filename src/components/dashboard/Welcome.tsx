da878b2e52f63302aef3543bbef0f86122af9424
Gentlecoder1 <israeloloruntoba3@gmail.com>
2026-01-28 13:15:45 +0100

    refactor(Welcome): adjust layout and improve dark mode support
    - Added margin-bottom to the main container for better spacing.
    - Updated visibility classes to include medium screens for the layout.
    - Enhanced dark mode text colors for better contrast and readability.
    - Adjusted image positioning for improved layout aesthetics.

--- src/components/dashboard/Welcome.tsx
+++ src/components/dashboard/Welcome.tsx
@@ -27,8 +27,8 @@ const options = [
 
 const Welcome: React.FC = () => {
   return (
-    <div className="w-full min-h-screen">
-      <MotionContainer className={`transition-all duration-300 `}>
+    <div className="w-full min-h-screen mb-10">
+      <MotionContainer className={`transition-all duration-300`}>
         <Header description="Browse Our Food Menu" navbarTitle="Gbam Gbistro" />
         <div className="w-full pt-[60px] lg:pt-[120px] px-6 lg:px-[42px]">
           <PopIn className="heading-font text-(--neutral-800) font-medium text-[22px] text-center dark:text-white lg:hidden">
@@ -36,18 +36,16 @@ const Welcome: React.FC = () => {
           </PopIn>
 
           <div
-            className="w-full hidden lg:flex lg:items-stretch lg:justify-between 
-  bg-(--neutral-900) dark:bg-(--neutral-150) 
-  rounded-[28.67px] px-[50px] "
+            className="w-full hidden md:flex md:items-stretch md:justify-between bg-(--neutral-900) dark:bg-(--neutral-150) rounded-[28.67px] px-[50px] "
           >
             {/* Text */}
             <FadeIn className="space-y-6 py-[50px]">
               <h1 className="text-[32px]">
-                <span className="heading-font text-white font-normal">
+                <span className="heading-font text-white dark:text-(--neutral-800) font-normal">
                   Welcome to
                 </span>
                 <br />
-                <span className="font-bold heading-font text-white">
+                <span className="font-bold heading-font text-white dark:text-(--neutral-800)">
                   Gram Bistro Restaurant 📍
                 </span>
               </h1>
@@ -55,14 +53,14 @@ const Welcome: React.FC = () => {
               <div className="flex flex-col xl:flex-row gap-2 xl:gap-6">
                 <p className="flex items-center gap-2">
                   <CiLocationOn size={24} className="text-(--yellow-1)" />
-                  <span className="whitespace-nowrap text-(--neutral-400) font-medium text-base">
+                  <span className="whitespace-nowrap text-(--neutral-400)  dark:text-(--neutral-600) font-medium text-base">
                     790 8th Ave, New York
                   </span>
                 </p>
 
                 <p className="flex items-center gap-2">
                   <IoTimeOutline size={24} className="text-(--yellow-1)" />
-                  <span className="whitespace-nowrap text-(--neutral-400) font-medium text-base">
+                  <span className="whitespace-nowrap text-(--neutral-400)  dark:text-(--neutral-600) font-medium text-base">
                     Mon - Sun: 12AM - 10PM
                   </span>
                 </p>
@@ -70,11 +68,11 @@ const Welcome: React.FC = () => {
             </FadeIn>
 
             {/* Image */}
-            <PopIn className="max-w-[443px]">
+            <PopIn className="max-w-[443px] -mr-10 lg:mb-0">
               <img
                 src="/images/welcome-page-illustration.svg"
                 alt="Welcome Page Illustration"
-                className="w-full h-full object-cover"
+                className="w-full h-full"
               />
             </PopIn>
           </div>
